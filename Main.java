17f25a9f99b1f2b5ff72cfaef6c204b508640e71
David Tovar <davidtovar2@u.boisestate.edu>
2018-03-24 17:11:32 -0600

    Revision 3 in branch

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 public class Main
 {
-	//this is the chronological revision 2 in master
+	//this is the chronological revision 3 in master
 
 	public void mehtodInMaster()
 	{
 		System.out.println("Revision 1 in master");
 		System.out.println("Revision 2 in master");
+	}
+
+	public void mehtodInBranch()
+	{
+		System.out.println("Revision 3 in branch");
 	}
 }
